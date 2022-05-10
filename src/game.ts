21bcdbb3e892b1c07628883ee75c18b4c9da66e2
iwaken71 <iwaken59371518@gmail.com>
2022-05-10 20:38:30 +0900

    first commit

---
+++ src/game.ts
@@ -0,0 +1,62 @@
+import * as BABYLON from 'babylonjs';
+import * as GUI from 'babylonjs-gui';
+
+export class Game {
+
+    private _canvas: HTMLCanvasElement;
+    private _engine: BABYLON.Engine;
+    private _scene: BABYLON.Scene;
+    private _camera: BABYLON.ArcRotateCamera;
+    private _light: BABYLON.Light;
+
+    constructor(canvasElement: string) {
+        // Create canvas and engine
+        this._canvas = <HTMLCanvasElement>document.getElementById(canvasElement);
+        this._engine = new BABYLON.Engine(this._canvas, true);
+    }
+
+    /**
+     * Creates the BABYLONJS Scene
+     */
+    createScene(): void {
+        // create a basic BJS Scene object
+        this._scene = new BABYLON.Scene(this._engine);
+        // create a FreeCamera, and set its position to (x:0, y:5, z:-10)
+        this._camera =  new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), this._scene);
+
+        this._camera.setTarget(BABYLON.Vector3.Zero());
+
+        this._camera.attachControl(this._canvas, true);
+        // create a basic light, aiming 0,1,0 - meaning, to the sky
+        this._light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), this._scene);
+
+        this._light.intensity = 0.7;
+
+        var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, this._scene);
+
+        sphere.position.y = 1;
+
+        var ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 6, height: 6}, this._scene);
+
+    }
+
+
+    /**
+     * Starts the animation loop.
+     */
+    animate(): void {
+        this._scene.registerBeforeRender(() => {
+            let deltaTime: number = (1 / this._engine.getFps());
+        });
+
+        // run the render loop
+        this._engine.runRenderLoop(() => {
+            this._scene.render();
+        });
+
+        // the canvas/window resize event handler
+        window.addEventListener('resize', () => {
+            this._engine.resize();
+        });
+    }
+}
